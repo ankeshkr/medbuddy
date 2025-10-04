beae4048ee227f3fd024c4c7cdd2d380e986956e
Ankesh Kumar <ankeshkumar@Ankeshs-MacBook-Air.local>
2025-10-05 01:34:02 +0530

    change in databse url for render

--- backend/app.py
+++ backend/app.py
@@ -42,7 +42,13 @@ from jose import JWTError, jwt
 import uvicorn
 
 # --- CONFIG ---
-DATABASE_URL = "sqlite:///./meds.db"
+#DATABASE_URL = "sqlite:///./meds.db"
+import os
+
+# get path to repo root (parent of backend/) for render
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'meds.db')}"
+
 SECRET_KEY = "change-me-in-production"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
